b40884df1e60cb93255bab0340858ed0d9b1090b
Mekhriddin <mirzoevmisha7@gmail.com>
2025-01-29 08:04:16 +0100

    Revert "new commits name"
    This reverts commit c2ae807b891fee1b00ac6904e33f64033c6b0953.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1 className='text-4xl text-gray-200 font-bold mb-4'>
-        Приветствую в элитном клубt!
+        Приветствую в элитном клубе!
       </h1>
       <button className='bg-sky-900 hover:bg-sky-800 text-white font-bold py-2 px-4 rounded'>
         Начать общаться!
